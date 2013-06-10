d32304469bcf38b5d975fc6252e5b2a2e3974892
zxorro <dmukhin.work@gmail.com>
2013-06-09 20:03:05 -0600

    Update sleepwalker.h

--- src/sleepwalker/sleepwalker.h
+++ src/sleepwalker/sleepwalker.h
@@ -1,9 +1,10 @@
-
 #ifndef SLEEPWALKER_H_
 #define SLEEPWALKER_H_
 
 #define F_CPU 8000000UL // 8 MHz
 
-
+#define LED_RED_PIN  5
+#define LED_IR_PIN   8
+#define POLL_FREQ_HZ 50
 
 #endif /* SLEEPWALKER_H_ */
